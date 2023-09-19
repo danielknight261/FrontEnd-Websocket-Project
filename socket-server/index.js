3cea016fd772f150b250fabceabaf5176fbc6d33
Dan Knight <danielknight261@gmail.com>
2023-09-19 11:02:53 +0100

    Intial Commit

---
+++ socket-server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const expressWs = require('express-ws');
+const cors = require('cors');
+
+const app = express();
+const wsInstance = expressWs(app);
+
+app.use(cors({
+  origin: 'http://localhost:3001', // Adjust this to your frontend URL
+}));
+
+app.ws('/', (ws, req) => {
+  console.log('Client connected to WebSocket');
+
+  const sendRandomNumber = () => {
+    const randomNumber = Math.floor(Math.random() * (9999999 - 1000000 + 1)) + 1000000;
+    ws.send(randomNumber.toString()); // Send the random number as a string to the client
+  };
+
+  const intervalId = setInterval(sendRandomNumber, 100);
+
+  ws.on('close', () => {
+    clearInterval(intervalId);
+    console.log('Client disconnected from WebSocket');
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Express server started on port 8080');
+});
